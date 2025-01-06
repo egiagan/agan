1c1efd7f76860785310df1afbcaac40e0b3b5228
egiagan <30861646+egiagan@users.noreply.github.com>
2025-01-06 09:27:51 +0700

    Update main-login.js

--- JS/main-login.js
+++ JS/main-login.js
@@ -63,6 +63,71 @@ var firebaseConfig = {
   
                 window.alert("Message : " + errorMessage);
             });
+var firebaseConfig = {
+  apiKey: "AIzaSyDAlth1sxiP-S3r3fVDXhwadVVnEvpdO6s",
+  authDomain: "login-egi-agan.firebaseapp.com",
+  databaseURL: "https://login-egi-agan-default-rtdb.firebaseio.com",
+  projectId: "login-egi-agan",
+  storageBucket: "login-egi-agan.firebasestorage.app",
+  messagingSenderId: "395059466114",
+  appId: "1:395059466114:web:5c6b0621e9739df6b5c99b"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.auth.Auth.Persistence.LOCAL;
+
+  $("#btn-login").click(function()
+  {
+      var email = $("#email").val();
+      var password = $("#password").val();
+
+      if(email != ""  &&  password != "")
+      {
+          var result = firebase.auth().signInWithEmailAndPassword(email, password); 
+
+          result.catch(function(error)
+          {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+
+              console.log(errorCode);
+              onsole.log(errorMessage);
+
+              window.alert("Message : " + errorMessage);
+          });
+      }
+      else
+      {
+          window.alert("Form is incomplete. Please fill out all fields.");
+      }
+  });
+
+
+
+
+  $("#btn-signup").click(function()
+  {
+      var email = $("#email").val();
+      var password = $("#password").val();
+      var cPassword = $("#confirmPassword").val();
+
+      if(email != ""  &&  password != ""  &&  cPassword != "")
+      {
+          if(password == cPassword)
+          {
+            var result = firebase.auth().createUserWithEmailAndPassword(email, password); 
+
+            result.catch(function(error)
+            {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+  
+                console.log(errorCode);
+                onsole.log(errorMessage);
+  
+                window.alert("Message : " + errorMessage);
+            });
           }
           else
           {
@@ -73,30 +138,108 @@ var firebaseConfig = {
       {
           window.alert("Form is incomplete. Please fill out all fields.");
       }
-    function reg_account(){
-  document.getElementById("registration-div").style.display = "block";
-  document.getElementById("login-div").style.display = "none";
-  document.getElementById("send-verification-div").style.display = "none";
-}
-
-function send_verification(){
-
-	var user = firebase.auth().currentUser;
-
-    user.sendEmailVerification().then(function() {
-      // Email sent.
-      //window.alert("Verification url sent.");
-     }).catch(function(error) {
-      // An error happened.
-      window.alert("Error "+ errorMessage);
-     });
-}
-function myFunction_reload() {
-    location.reload();
-}
   });
 
 
+
+  $("#btn-resetPassword").click(function()
+  {
+     var auth = firebase.auth();
+     var email = $("#email").val();
+
+     if(email != "")
+     {
+         auth.sendPasswordResetEmail(email).then(function()
+         {
+            window.alert("Email has been sent to you, Please check and verify.");
+         })
+         .catch(function(error)
+         {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+
+            console.log(errorCode);
+            onsole.log(errorMessage);
+
+            window.alert("Message : " + errorMessage);
+         });
+     }
+     else
+     {
+        window.alert("Please write your email first.");
+     }
+  });
+
+
+
+  $("#btn-logout").click(function()
+  {
+     firebase.auth().signOut();
+  });
+
+
+
+
+  $("#btn-update").click(function()
+  {
+    var phone = $("#phone").val();
+    var address = $("#address").val();
+    var bio = $("#bio").val();
+    var fName = $("#firstName").val();
+    var sName = $("#secondName").val();
+    var country = $("#country").val();
+    var gender = $("#gender").val();
+
+    var rootRef = firebase.database().ref().child("Users");
+    var userID = firebase.auth().currentUser.uid;
+    var usersRef = rootRef.child(userID);
+
+    if(fName!="" && sName!="" && phone!="" && country!="" && gender!="" && bio!="" && address!="")
+    {
+        var userData = 
+        {
+            "phone": phone,
+            "bio": bio,
+            "firstName": fName,
+            "secondName": sName,
+            "country": country,
+            "gender": gender,
+        };
+
+        usersRef.set(userData, function(error)
+        {
+            if(error)
+            {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode);
+                onsole.log(errorMessage);
+
+                window.alert("Message : " + errorMessage);
+            }
+            else
+            {
+                window.location.href = "index.html";
+            }
+        });
+    }
+    else
+    {
+        window.alert("Form is incomplete. Please fill out all fields.");
+    }
+  });
+
+
+function switchView(view)
+{
+    $.get({
+        url:view,
+        cache:false,
+    })
+    .then(function(data){
+        $("#container").html(data);
+    });
 
   $("#btn-resetPassword").click(function()
   {
