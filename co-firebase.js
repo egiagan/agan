5c62221fd0577b014a378110b119c84ad8bd481b
egiagan <30861646+egiagan@users.noreply.github.com>
2025-01-06 19:59:04 +0700

    Create co-firebase.js

---
+++ co-firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDAlth1sxiP-S3r3fVDXhwadVVnEvpdO6s",
+    authDomain: "login-egi-agan.firebaseapp.com",
+    databaseURL: "https://login-egi-agan-default-rtdb.firebaseio.com",
+    projectId: "login-egi-agan",
+    storageBucket: "login-egi-agan.firebasestorage.app",
+    messagingSenderId: "395059466114",
+    appId: "1:395059466114:web:5c6b0621e9739df6b5c99b"
+};
